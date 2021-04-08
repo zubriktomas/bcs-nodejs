b51655717888665e2bf867c481fd17a2c959bbed
zubriktomas <zubriktomas@gmail.com>
2021-04-08 19:29:56 +0200

    Box vizualizion generalized, refactored

--- src/modules/box-vizualizer.js
+++ src/modules/box-vizualizer.js
@@ -52,78 +52,58 @@ function createSvgRepresentation(data) {
   });
 }
 
-// Create visual (SVG) representation of given box structure
-function createSvgBox(box) {
+function createSvgRect(entity, props) {
 
   svg.rect({
-    x: box.left,
-    y: box.top,
-    width: box.width,
-    height: box.height,
-    fill: box.color,
-    padding:0,
-    margin:0
-  });
-}
-
-function createSvgClusterBox(cluster, type) {
-  var width = cluster.right - cluster.left;
-  var height = cluster.bottom - cluster.top;
-  
-  svg.rect({
-    x: cluster.left,
-    y: cluster.top,
-    width: width,
-    height: height,
-    fill: 'none',
-    stroke: !type ? '#FF0000': type=='cluster' ? '#2FFF7A' : '#f5e942', // green|red|cyan
-    'stroke-width':  type && type=='box' ? 2 : 3,
-    'stroke-opacity': type && type=='cluster' ? 0.5 : 1.0,  
-    padding: 0,
-    margin:0
+    x: props.x || entity.left,
+    y: props.y || entity.top,
+    width: props.width || entity.width || entity.right - entity.left,
+    height: props.height || entity.height || entity.bottom - entity.top ,
+    fill: props.fill || 'none',
+    stroke: props.stroke || 'none',
+    'stroke-width':  props.strokeWidth || 0,
+    'stroke-opacity': props.strokeOpacity || 1,
+    padding: props.padding || 0,
+    margin: props.margin || 0
   });
 }
 
 // Create box representation of webpage nodes
 function buildSvg(req, data) {
 
-  var boxes, clusters, header;
-  
-  header = `<style> body { margin: 0; padding:0; } </style>`;
+  var header = `<style> body { margin: 0; padding:0; } </style>`;
 
   svg.width(data.document.width);
   svg.height(data.document.height);
 
-  if(data.hasOwnProperty('boxes')){
-    boxes = Array.isArray(data.boxes) ? data.boxes : Object.values(data.boxes);
-    boxes.forEach(box => {
-      createSvgBox(box);  
-    });
+  if(data.boxes){
+    for (const box of data.boxes) {
+      createSvgRect(box, {fill: box.color});
+    }
   }
   
-  if(data.hasOwnProperty('clusters')) {
-    clusters = Array.isArray(data.clusters) ? data.clusters : Object.values(data.clusters);
-    clusters.forEach(cluster => {
-      createSvgClusterBox(cluster);
-    });
+  if(data.clusters) {
+    for (const cluster of data.clusters) {
+      createSvgRect(cluster, {stroke: '#000000', strokeWidth: 3}); //stroke color: BLACK
+    }
   }
   
-  if(data.hasOwnProperty('entityA')) {
-    if(data.entityA) {
-      createSvgClusterBox(data.entityA, 'box');
+  if(data.entityA) {
+    createSvgRect(data.entityA, {stroke: '#2FFF7A', strokeWidth: 2});  //stroke color: GREEN
+  }
+
+  if(data.entityB) {
+    createSvgRect(data.entityB, {stroke: '#2FFF7A', strokeWidth: 2}); //stroke color: GREEN
+  }
+
+  if(data.neighbours) {
+    for (const neighbour of data.neighbours) {
+      createSvgRect(neighbour, {stroke: '#fc03df', strokeWidth: 2}); // color: MAGENTA
     }
   }
 
-  if(data.hasOwnProperty('entityB')) {
-    if(data.entityB) {
-      createSvgClusterBox(data.entityB, 'box');
-    }
-  }
-
-  if(data.hasOwnProperty('cc')) {
-    if(data.cc) {
-      createSvgClusterBox(data.cc, 'cluster');
-    }
+  if(data.cc) {
+    createSvgRect(data.cc, {stroke: '#FF0000', strokeWidth: 3});
   }
 
   return `<!DOCTYPE html>
